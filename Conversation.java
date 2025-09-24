e893ebf05887cc2038b6820ad0b5317256c74c96
Zhuoyan Fang <zfang@smith.edu>
2025-09-23 20:21:39 -0400

    Assignment finished.

--- Conversation.java
+++ Conversation.java
@@ -1,36 +1,132 @@
-
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 class Conversation implements ConversationRequirements {
 
   // Attributes 
+  ArrayList<String> transcript;
+  int position;
 
   /**
    * Constructor 
+   * @param transcript it is the whole records of all inputs and outputs
+   * @param position record where we are in the transcript
    */
   Conversation() {
-    
+    transcript = new ArrayList<String>(); 
+    position = 0;
   }
 
   /**
    * Starts and runs the conversation with the user
+   * @param round record the chatting rounds
+   * @param greeting the greeting from the chatbot
+   * @param inputString the reaction we give to the chatbot
+   * @param reply the reply for the inputString
+   * @param goodbye the goodbye after the conversation
    */
   public void chat() {
+    Scanner input=new Scanner(System.in);
 
+    System.out.println("How many rounds?");
+    int round=input.nextInt();
+    input.nextLine();
+
+    String greeting="Hi there! What's on your mind?";
+    System.out.println(greeting);
+    transcript.add(greeting);
+    position= position + 1;
+
+  
+    for (int i= 0; i < round; i++) {
+      String inputString= input.nextLine();
+      transcript.add(inputString);
+      position= position + 1;
+
+
+      String reply= respond(inputString);
+      System.out.println(reply);
+      transcript.add(reply);
+      position= position + 1;
+    }
+
+    String goodbye= "See ya!";
+    System.out.println(goodbye);
+     transcript.add(goodbye);
+    position= position+1;
+    input.close();
   }
 
   /**
    * Prints transcript of conversation
+   * @param transcript the whole records of all inputs and outputs
    */
   public void printTranscript() {
-
+    System.out.println();
+    System.out.println("TRANSCRIPT:");
+    for (int i=0; i<position; i++){
+      System.out.println(transcript.get(i));
+    }
+    
   }
 
   /**
    * Gives appropriate response (mirrored or canned) to user input
    * @param inputString the users last line of input
+   * @param lastWord the last word of the whole sentence
+   * @param words each word in the input sentence
+   * @param mirrored whether there are pronouns that we need to mirror in the sentences
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
+    String[] words= inputString.split(" ");
+    boolean mirrored=false;
     String returnString = ""; 
+    String lastWord=words[words.length - 1];
+
+    if (lastWord.charAt(lastWord.length() - 1) == '.'||lastWord.charAt(lastWord.length() - 1) == '?'||lastWord.charAt(lastWord.length() - 1) == '!'){
+     words[words.length - 1] = lastWord.substring(0, lastWord.length() - 1);
+    }
+    for (int i=0; i<words.length;i++){
+      String word=words[i].toLowerCase();
+      if ("i".equals(word)){
+        words[i]= "you";
+        mirrored=true; 
+      } else if ("me".equals(word)){
+        words[i]= "you";
+        mirrored=true; 
+      } else if ("am".equals(word)){
+        words[i]= "are";
+        mirrored=true; 
+      } else if ("you".equals(word)){
+        if (i==0){
+          words[i]= "I";
+        } else {
+          words[i]= "me";
+        }
+        mirrored=true; 
+      } else if ("my".equals(word)){
+        words[i]= "your";
+        mirrored=true; 
+      } else if ("your".equals(word)){
+        words[i]= "my";
+        mirrored=true; 
+      }  else if ("i'm".equals(word)){
+        words[i]= "you're";
+        mirrored=true; 
+      } else if ("you're".equals(word)){
+        words[i]= "I'm";
+        mirrored=true; 
+      } 
+    }
+    if (mirrored==true){
+        returnString=String.join(" ",words)+"?";
+        returnString=returnString.substring(0,1).toUpperCase()+returnString.substring(1);
+    }else{
+      String[] canned={"Mmm-hm.","I see.","Interesting!","Wow.","Tell me more about that!"};
+      Random rand=new Random(); 
+      returnString= canned[rand.nextInt(canned.length)];
+    }
     return returnString; 
   }
 
--- reflection.md
+++ reflection.md
@@ -1,7 +1,17 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I consulted with Amy Tang and Cola Shi while working on this assignment.
+
 - Which session(s) of TA / office hours did you attend?
+I attened the TA hours in Tuesday 7-9PM EST.
+
 - What are your initial impressions of Java? 
+I think it is interesting and challenging for me at first. It has some similarities with Python, but it is also hard for me to change the rules that I follow in Python into Java. However, after I get more familiar with it, I find that Java is a language that is very organzied and easy to go with.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+For me, Python does not need to add the types of the parameters everytime when we defining a new parameter but Java does. However,they have the same logic like using loops, printing, and defining the functions.
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+At first I cannot successfully mirroring the names when they are at the end of a sentence. This is because they are ending with the punctuation. After I remove the punctuations of them, I have run the code successfully.
+
